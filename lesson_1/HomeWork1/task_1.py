a204b2c20e795eff9a3af62773fcfbc81740e8a0
elena_s <esonina@icloud.com>
2022-09-24 23:49:15 +0300

    add task_1 days of the week

---
+++ lesson_1/HomeWork1/task_1.py
@@ -0,0 +1,10 @@
+# Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+print('введите день недели ')
+day = int(input()) 
+if day in range(6):
+     print('Workday')
+elif day in range(6,8): 
+    print ('Weekend')
+else:
+    print ("It's not a day of the week!")
