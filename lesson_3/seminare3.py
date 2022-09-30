7c1d29ede1c64fdc1e5c42083d96d1031f3b0452
elena_s <esonina@icloud.com>
2022-09-30 23:03:58 +0300

    add seminare3.py

---
+++ lesson_3/seminare3.py
@@ -0,0 +1,39 @@
+ # 1. Задайте список, состоящий из произвольных чисел, количество задаёт пользователь.
+# Напишите программу, которая определит, присутствует ли в заданном списке число,
+# полученное от пользователя.
+
+# from random import sample
+
+# def num_in_list(count, number):
+#     if count < 0:
+#         return "Error"
+#     my_list = sample(range(1, count *2), count)
+#     print(my_list)
+#     if number in my_list:
+#         return "Yes"
+#     return "No"
+# print(num_in_list(int(input()), int(input())))
+
+# 2. Задайте список, состоящий из произвольных слов, количество задаёт пользователь.
+# Напишите программу, которая определит индекс второго вхождения строки в списке
+# либо сообщит, что её нет.
+
+from random import choices
+
+def list_new(n, word):
+    new_list = []
+    for i in range(n):
+        a = choices(word, k=3)
+        new_list.append(''.join(a))
+    return new_list
+
+def list_search(my_list, key):
+    if my_list.count(key) >1:
+        print("Yes")
+        n=my_list.index(key)
+        print(my_list.index(key, n+1))
+    else:
+        print(-1)
+result=list_new(10, 'abc')
+print(result)
+list_search(result, input())
